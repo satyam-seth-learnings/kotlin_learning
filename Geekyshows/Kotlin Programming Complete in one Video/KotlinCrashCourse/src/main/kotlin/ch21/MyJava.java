2b6afec01830256736d1f7f430aeff83aaf663ff
satyam-seth <satyam1998.1998@gmail.com>
2023-10-18 01:10:12 +0530

    add ch21

---
+++ Geekyshows/Kotlin Programming Complete in one Video/KotlinCrashCourse/src/main/kotlin/ch21/MyJava.java
@@ -0,0 +1,13 @@
+package ch21;
+
+/*** Calling Java from Kotlin ***/
+public class MyJava {
+    private int a;
+
+    public int getValue(){
+        return a;
+    }
+    public void setValue(int value){
+        this.a = value;
+    }
+}
---
+++ Geekyshows/Kotlin Programming Complete in one Video/KotlinCrashCourse/src/main/kotlin/ch21/MyKotlin.kt
@@ -0,0 +1,8 @@
+import ch21.MyJava
+
+/*** Calling Java from Kotlin ***/
+fun main() {
+    val obj = MyJava()
+    obj.setValue(10)
+    println(obj.getValue())
+}
